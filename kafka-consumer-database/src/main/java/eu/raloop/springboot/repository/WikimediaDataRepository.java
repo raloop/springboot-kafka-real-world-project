dc821e2fd24b62274ac986db2fbb3186fa33d1d9
raloop <luprares@gmail.com>
2023-06-12 17:00:28 +0300

    adding the files

---
+++ kafka-consumer-database/src/main/java/eu/raloop/springboot/repository/WikimediaDataRepository.java
@@ -0,0 +1,7 @@
+package eu.raloop.springboot.repository;
+
+import eu.raloop.springboot.entity.WikimediaData;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WikimediaDataRepository extends JpaRepository<WikimediaData, Long> {
+}
